7a3bb501571d6ae782a4970bd5e9729ef0272212
hulyayldz <hulya0898yildiz@gmail.com>
2025-04-27 22:48:21 +0300

    Create Employee.java

---
+++ src/main/java/com/workintech/s19d4/entity/Employee.java
@@ -0,0 +1,29 @@
+package com.workintech.s19d4.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(schema = "fsweb", name = "employee")
+public class Employee {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "first_name")
+    private String firstName;
+
+    @Column(name = "last_name")
+    private String lastName;
+
+    @Column(name = "email")
+    private String email;
+
+    @Column(name = "salary")
+    private Double salary;
+}
